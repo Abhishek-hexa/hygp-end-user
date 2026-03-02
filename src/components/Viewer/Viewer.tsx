6805da4074f94f389d9ea04e65e110ef711b18a3
Parth Chauhan <parthchau@hexacoder.com>
2026-03-02 19:02:02 +0530

    chore: setup tailwind and removed mui

--- src/components/Viewer/Viewer.tsx
+++ src/components/Viewer/Viewer.tsx
@@ -1,24 +1,14 @@
-import { Box } from '@mui/material';
 import { observer } from 'mobx-react-lite';
 
 import { NavBar } from './NavBar/NavBar';
 
 export const Viewer = observer(() => {
   return (
-    <Box sx={{ bgcolor: 'white', height: '100vh', width: '100%' }}>
+    <div className="h-screen w-full bg-white">
       <NavBar />
-      <Box
-        sx={{
-          alignItems: 'center',
-          color: '#444',
-          display: 'flex',
-          height: 'calc(100vh - 64px)',
-          justifyContent: 'center',
-          marginTop: '64px',
-          width: '100%',
-        }}>
+      <div className="mt-16 flex h-[calc(100vh-64px)] w-full items-center justify-center text-[#444]">
         3D rendering is temporarily disabled.
-      </Box>
-    </Box>
+      </div>
+    </div>
   );
 });
