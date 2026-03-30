cf13c70c379b84e91bff6307ddab22478e1fcebd
Parth Chauhan <parthchau@hexacoder.com>
2026-03-30 13:03:34 +0530

    fix: removed initializeDogCollarApis.ts

--- src/api/initializeDogCollarApis.ts
+++
@@ -1,8 +0,0 @@
-import { ProductManager } from '../state/product/ProductManager';
-import { UiManager } from '../state/ui/UiManager';
-import { initializeProductApis } from './initializeProductApis';
-
-export const initializeDogCollarApis = async (
-  productManager: ProductManager,
-  uiManager: UiManager,
-) => initializeProductApis(productManager, uiManager, 'DOG_COLLAR');
