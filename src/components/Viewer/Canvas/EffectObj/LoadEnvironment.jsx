eca5fe6935e8c9d542d3598fe85e2537ac65b401
Abhishek-Hexa <abhishek@hexacoder.com>
2026-03-31 19:00:22 +0530

    refactor: simplify LoadEnvironment component by removing unused texture loading logic

--- src/components/Viewer/Canvas/EffectObj/LoadEnvironment.jsx
+++ src/components/Viewer/Canvas/EffectObj/LoadEnvironment.jsx
@@ -1,34 +1,14 @@
 import { Environment } from '@react-three/drei';
 import { observer } from 'mobx-react-lite';
-import { useEffect, useState } from 'react';
-import * as THREE from 'three';
-import { loadHdrEnvMapCached } from './hdrEnvMapCache';
 
 export const LoadEnvironment = observer(() => {
-    const [texture, setTexture] = useState(new THREE.Texture());
-
-    useEffect(() => {
-        loadHdrEnvMapCached('/assets/texture/texture/metro_vijzelgracht_1k2.hdr').then((texture) => {
-            texture.mapping = THREE.EquirectangularReflectionMapping;
-            texture.magFilter = THREE.LinearFilter;
-            texture.minFilter = THREE.NearestFilter;
-            setTexture(texture);
-        });
-    }, []);
-    return (
-        <Environment>
-            <mesh rotation={[-1.86, 0.78, 0.06]} scale={100}>
-                <sphereGeometry />
-                <meshBasicMaterial
-                    transparent
-                    opacity={1}
-                    map={texture ? texture : null}
-                    side={THREE.BackSide}
-                    toneMapped={false}
-                />
-            </mesh>
-        </Environment>
-    );
+  return (
+    <Environment
+      files="/assets/texture/texture/metro_vijzelgracht_1k2.hdr"
+      environmentRotation={[-1.72, 0.13, -0.05]}
+      resolution={1024}
+    />
+  );
 });
 
 export default LoadEnvironment;
