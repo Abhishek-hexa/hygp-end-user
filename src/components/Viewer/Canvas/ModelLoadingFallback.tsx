977cabd4de7c8806ea6b91bdf64fffe311781bc4
Parth Chauhan <parthchau@hexacoder.com>
2026-04-01 14:40:37 +0530

    fix: remove conditional hook calling from fallback

--- src/components/Viewer/Canvas/ModelLoadingFallback.tsx
+++ src/components/Viewer/Canvas/ModelLoadingFallback.tsx
@@ -1,14 +1,13 @@
-import { observer } from "mobx-react-lite";
-import { useEffect } from "react";
-import { useMainContext } from "../../../hooks/useMainContext";
+import { observer } from 'mobx-react-lite';
+import { useEffect } from 'react';
+import { useMainContext } from '../../../hooks/useMainContext';
 
 export const ModelLoadingFallback = observer(() => {
   const { uiManager, designManager } = useMainContext();
   const id = designManager.productManager.activeModelKey;
 
-  if(!id) return;
-  
   useEffect(() => {
+    if (!id) return;
     uiManager.add3DLoadingItem(id);
     return () => {
       uiManager.remove3DLoadingItem(id);
